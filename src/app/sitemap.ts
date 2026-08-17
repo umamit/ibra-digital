20a5d1bb5999d8c6cae50374762e0c2f8b52ddcf
umamit <shot.ann09@gmail.com>
2026-08-18 02:24:20 +0900

    seo: update homepage crawl frequency to weekly in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://ibradigital.id",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "weekly",
       priority: 1,
     },
     {
