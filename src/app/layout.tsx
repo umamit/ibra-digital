b702f681bde29ced01206d167ab944df5fa14577
umamit <shot.ann09@gmail.com>
2026-08-18 02:33:59 +0900

    fix: import Flaticon Uicons stylesheet to render icons on the site and add eslint config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/@flaticon/flaticon-uicons@3.3.1/css/all/all.min.css" rel="stylesheet" />
       </head>
       <body>{children}</body>
     </html>
